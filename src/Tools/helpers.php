cd1490bdbad9fb1707db6e356a8afd68589a8f5b
Sofiane Akbly <sofiane.akbly@gmail.com>
2021-03-10 15:22:36 +0100

    Ajout de fonctions

--- src/Tools/helpers.php
+++ src/Tools/helpers.php
@@ -3,6 +3,7 @@
 use Carbon\Carbon;
 use Ssf\Support\Config;
 use Ssf\Support\Env;
+use Ssf\Support\Log;
 
 if (!function_exists('env')) {
     /**
@@ -53,5 +54,56 @@ if (!function_exists('today')) {
     function today($format = null)
     {
         return $format ? Carbon::now()->format($format) : Carbon::now();
+    }
+}
+
+if (!function_exists('toObject')) {
+    /**
+     * @param mixed $data
+     * @return mixed|null
+     */
+    function toObject($data)
+    {
+        return $data ? json_decode(json_encode($data)) : null;
+    }
+}
+
+if (!function_exists('toArray')) {
+    /**
+     * @param mixed $data
+     * @return array|null
+     */
+    function toArray($data)
+    {
+        return $data ? json_decode(json_encode($data), true) : null;
+    }
+}
+
+if (!function_exists('get_caller_method')) {
+    /**
+     * @param bool $class
+     * @return mixed|null
+     */
+    function get_caller_method($class = false)
+    {
+        $dbt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3);
+        return isset($dbt[2]['function'])
+            ? ($class ? $dbt[2]['class'] . '::' . $dbt[2]['function'] : $dbt[2]['function'])
+            : null;
+    }
+}
+
+if (!function_exists('signal')) {
+    /**
+     * @param Throwable $e
+     * @param bool $quiet
+     */
+    function signal(Throwable $e, bool $quiet = false)
+    {
+        if ($quiet === false && php_sapi_name() == "cli") {
+            echo PHP_EOL . "\033[01;33m" . $e->getFile() . "(" . $e->getLine() . ") : \033[0m" . PHP_EOL . PHP_EOL;
+            echo "\033[01;31m" . $e->getMessage() . "\033[0m" . PHP_EOL . PHP_EOL;
+        }
+        Log::error($e);
     }
 }
